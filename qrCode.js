c7dfafb6b16505a0eb34094c2126565d4509639f
tomas.pvm.33@gmail.com <tomas.pvm.33@gmail.com>
2023-11-29 22:12:06 -0300

    arreglo

--- qrCode.js
+++ qrCode.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var enlaceDescarga = "https://drive.google.com/drive/folders/1EBHqjDiz0EiwABJYzjgY7M75Hr11oP7_"; 
+    var enlaceDescarga = "https://github.com/TomasPaz22/DescargarAPK/releases/download/Apk/app-release.apk"; 
     var qrcode = new QRCode(document.getElementById("codigoQR"), {
         text: enlaceDescarga,
         width: 256,
